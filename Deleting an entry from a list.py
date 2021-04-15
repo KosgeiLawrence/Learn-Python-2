fbad1c1e601be3d8e14e313f1765522f81107eb0
Lawrence Kosgei <82581644+KosgeiLawrence@users.noreply.github.com>
2021-04-15 13:37:03 -0700

    Add files via upload

---
+++ Deleting an entry from a list.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 22:17:40 2019
+
+@author: Lawrence
+"""
+
+guest= ['Ian','Amon','Lawrence','Samwel']
+del guest[0]
+print(guest[0])
---
+++ Desplaying the guests in the lists despite not given their number in the list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 22:45:26 2019
+
+@author: Lawrence
+"""
+
+guest= ['Ian','Amon','Lawrence','Samwel']
+nmberofguests=len(guest)
+for steps in range(nmberofguests):
+    print(guest[steps])
